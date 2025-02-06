673de9a905b3f3bd8f6eb01e188e5a3a3c7316f7
Aksheen Bhinder <bhindeak@sheridancollege.ca>
2025-02-05 20:18:56 -0500

    set up build gradle , test

---
+++ src/test/java/ca/sheridancollege/bhindeak/onlinebookstore/OnlineBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.bhindeak.onlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
